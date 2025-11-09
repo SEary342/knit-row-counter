698989b7f379a1ab827a795fa1cce28341f42b4d
Sam Eary <samwise342@gmail.com>
2025-11-09 09:53:39 -0500

    Lint Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
+
 import Layout from './components/Layout'
 import ProjectPickerView from './pages/ProjectPickerView'
 import ProjectView from './pages/ProjectView'
