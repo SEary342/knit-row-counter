f0a89283f69bb7e62da00e51c3450e587ecdc753
Sam Eary <samwise342@gmail.com>
2025-11-13 18:21:39 -0500

    Fixes #1 Hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import { store } from './app/store'
 import App from './App'
@@ -9,9 +9,9 @@ import App from './App'
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={import.meta.env.ROUTER_BASENAME}>
+      <HashRouter basename={import.meta.env.ROUTER_BASENAME}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
